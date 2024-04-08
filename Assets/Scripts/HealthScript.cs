de28d11f43cbe53997857067e7cb283c57e3406e
dianaMarin03 <marindiana724@yahoo.com>
2024-04-08 20:07:32 +0300

    Added script for health logic

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -1,15 +1,56 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
-    
+    public GameObject Health1;
+    public GameObject Health2;
+    public GameObject Health3;
+    private CollisionHandler collisionHandler;
+
+    private int lives;
     void Start()
     {
-
+        Health1.gameObject.SetActive(false);
+        Health2.gameObject.SetActive(false);
+        Health3.gameObject.SetActive(false);
+        collisionHandler = FindAnyObjectByType(typeof(CollisionHandler)).GetComponent<CollisionHandler>();
     }
 
     void Update()
     {
-        
+        lives = collisionHandler.GetLivesRemaining();
+        if (lives >= 0)
+        {
+            switch (lives)
+            {
+                case 0:
+                    {
+                        Health1.gameObject.SetActive(false);
+                        Health2.gameObject.SetActive(false);
+                        Health3.gameObject.SetActive(false);
+                        break;
+                    }
+                case 1:
+                    {
+                        Health1.gameObject.SetActive(false);
+                        Health2.gameObject.SetActive(false);
+                        break;
+                    }
+                case 2:
+                    {
+                        Health1.gameObject.SetActive(false);
+                        break;
+                    }
+            }
+        }
     }
+
+    public void DisplayHealth()
+    {
+        Health1.gameObject.SetActive(true);
+        Health2.gameObject.SetActive(true);
+        Health3.gameObject.SetActive(true);
+    }
+
 }
